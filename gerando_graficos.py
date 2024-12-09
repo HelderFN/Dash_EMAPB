9d1b16dba6659305eb84e4b46256226d98b55e4e
Helder <helderfn@outlook.com>
2024-12-09 11:14:16 -0300

    atualizando

--- gerando_graficos.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import tratamento_planilha as tp
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-
-df = pd.read_excel("data_frame_total.xlsx")
-print(df)
